bc181f1e66deeac60359d07592e62a53c5682b56
Pranavi Jain <pranviijain@gmail.com>
2024-03-22 23:06:42 -0700

    Output function base (#1)
    * Creating a new branch
    
    * Measuring circuit in different basis
    
    * Removing output
    
    * Minor changes
    
    * Restructuring code

---
+++ utils.py
@@ -0,0 +1,12 @@
+from qiskit.quantum_info.analysis import hellinger_fidelity
+
+"""Utility function to get fidelity for our output dictionary object - measuring 
+fidelity on matching basis"""
+
+def get_matched_fidelity(op1, op2, basis):
+    res1 = op1.get(basis)
+    res2 = op2.get(basis)
+    print("Circuit - "+str(res1))
+    print("Noisy C - "+str(res2))
+    fidelity = hellinger_fidelity(res1, res2)
+    return fidelity
